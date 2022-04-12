334ad5af1e2ecf893ef73d07ab6beb358453bc77
MilanaAa897@outlook.com <MilanaAa897@outlook.com>
2022-04-12 15:38:34 +0300

    добавлены комментарии

--- СтрингиЪ/Strings.cs
+++ СтрингиЪ/Strings.cs
@@ -8,6 +8,14 @@ namespace СтрингиЪ
     /// </summary>
     public class Strings
     {
+        /// <summary>
+        /// Индекс первого вхождения подстроки sub
+        /// в строку source, начиная с индекса start
+        /// </summary>
+        /// <param name="source">строка, в которой производится поиск</param>
+        /// <param name="sub">подстрока</param>
+        /// <param name="start">индекс начала проверки вхождения</param>
+        /// <returns>индекс первого вхождения подсроки в строку начиная с индекса start. Если вхождения нет, возвращает -1</returns>
         public static int MyIndexOf(string source, string sub, int start = 0)
         {
             int index = -1;
@@ -29,7 +37,12 @@ namespace СтрингиЪ
 
             return index;
         }
-
+        /// <summary>
+        /// поиск всех вхождений образца pattern в исходную строку source
+        /// </summary>
+        /// <param name="source">строка, в которой производится поиск</param>
+        /// <param name="pattern">образец</param>
+        /// <returns>список индексов начала вхождения</returns>
         public static List<int> AllIndexOf(string source, string pattern)
         {
             List<int> result = new List<int>();
@@ -44,7 +57,13 @@ namespace СтрингиЪ
 
             return result;
         }
-
+        /// <summary>
+        /// Вспомогательная функция метода КМП(Кнута, Морриса, Пратта)
+        /// эффективного поиска вхождения подстроки в строку
+        /// подсчёт длины префиксов, совпадающих с суффиксами
+        /// </summary>
+        /// <param name="pattern">образец для поиска</param>
+        /// <returns>массив длин префиксов</returns>
         static int[] PrefixFunction(string pattern)
         {
             int m = pattern.Length;
@@ -60,7 +79,13 @@ namespace СтрингиЪ
 
             return res;
         }
-
+        /// <summary>
+        /// Эффективный алгоритм КМП поиска вхождения  образца
+        /// pattern в строку source
+        /// </summary>
+        /// <param name="source">исходная строка</param>
+        /// <param name="pattern">образец для поиска</param>
+        /// <returns>Индекс первого вхождения. Если вхождения нет, вохвращает -1</returns>
         public static int IndexOf_KMP(string source, string pattern)
         {
             int n = source.Length;
@@ -83,6 +108,12 @@ namespace СтрингиЪ
             else return -1;
         }
 
+        /// <summary>
+        /// Список всех вхождений pattern в строку source
+        /// </summary>
+        /// <param name="source">строка, в которой производится поиск</param>
+        /// <param name="pattern">образец</param>
+        /// <returns>список всех вхождений</returns>
         public static List<int> IndexOfKMP_Best(string source, string pattern)
         {
             string expand = pattern + "#" + source;
--- СтрингиЪ_тесты/obj/Release/net5.0/СтрингиЪ_тесты.AssemblyInfo.cs
+++ СтрингиЪ_тесты/obj/Release/net5.0/СтрингиЪ_тесты.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("СтрингиЪ")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("СтрингиЪ_тесты")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("СтрингиЪ")]
-[assembly: System.Reflection.AssemblyTitleAttribute("СтрингиЪ")]
+[assembly: System.Reflection.AssemblyProductAttribute("СтрингиЪ_тесты")]
+[assembly: System.Reflection.AssemblyTitleAttribute("СтрингиЪ_тесты")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
